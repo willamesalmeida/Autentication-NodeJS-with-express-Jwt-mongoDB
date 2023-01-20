2a1818699316acdaeb9a0c00c14d9fd4754cd024
José Willames <willames5480@gmail.com>
2023-01-20 16:44:07 -0300

    firt commit

--- app.js
+++ app.js
@@ -167,7 +167,7 @@ const dbPassword = process.env.DB_PASS;
 
 mongoose
   .connect(
-    `mongodb+srv://willamesab:WlgaMK22GbRo34Eh@cluster0.i45m479.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://${dbUser}:${dbPassword}@cluster0.i45m479.mongodb.net/?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(3000);
